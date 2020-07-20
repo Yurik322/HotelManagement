a6c6aa6118715483f8396d6f312de26d7964e557
Yurii <fytruk14@gmail.com>
2020-07-20 19:52:14 +0300

    installed entity framework and enabled migrations

---
+++ HotelManagement.Data/HotelManagementContext.cs
@@ -0,0 +1,22 @@
+﻿using HotelManagement.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelManagement.Data
+{
+    public class HotelManagementContext : DbContext
+    {
+        public HotelManagementContext() : base("HMSConnectionString")
+        {
+        }
+
+        public DbSet<AccommodationType> AccommodationTypes { get; set; }
+        public DbSet<AccommodationPackage> AccommodationPackages { get; set; }
+        public DbSet<Accommondation> Accommondations { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
