51bce592ee9db3751d8d3b8decafbeb79648f4c4
Yurii <fytruk14@gmail.com>
2020-07-20 19:32:42 +0300

    initialized

---
+++ HotelManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HotelManagement.Startup))]
+namespace HotelManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
